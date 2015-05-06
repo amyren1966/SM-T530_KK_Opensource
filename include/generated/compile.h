015ce2be9e904c70c0f958b922fa82c716802900
Anbjorn <amyren1966@users.noreply.github.com>
2015-05-06 08:02:14 +0200

    Update compile.h

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 26 12:24:37 KST 2014"
-#define LINUX_COMPILE_BY "omsgsm"
-#define LINUX_COMPILE_HOST "sy918-desktop"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
+#define UTS_VERSION "#1 SMP PREEMPT Tue May 5 20:20:11 CEST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "amyren-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.9.3 20150117 (prerelease) (SaberMod-4.9.x_arm-graphite) "
